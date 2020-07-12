ec4b744b1ea0f5b082f87677c4c5496a3ad06fbd
Benjamin Luke Smith-Gordon <ben.smith.gordon@gmail.com>
2020-07-12 15:40:34 +0100

    Completed the setDate function so that the second hand rotates

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 function setDate() {
-  const now = new Date(); //grabs the current from the browser
-  const seconds = now.getSeconds(); //extracts the seconds from the date
+  const secondHand = document.querySelector('.second-hand');
+  const now = new Date(); // grabs the current date from the browser
+  const seconds = now.getSeconds(); // extracts the seconds from the date
+  const secondsDegrees = ((seconds / 60) * 360) + 90; // calculates degrees by doing some math
+  
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`; // rotates the seccond hand depending on the value of 'secondsDegrees'
 }
 
 setInterval(setDate, 1000);
